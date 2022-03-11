7f67a3aeaa6cb67a196e10db33d3435d18aa4035
hardik <hardik.essence@gmail.com>
2022-03-12 00:01:21 +0530

    Add storage session

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -2,33 +2,14 @@
 
 class AuthenticatedController < ApplicationController
   include ShopifyApp::Authenticated
-  # include ShopifyApp::EmbeddedApp
-  # include ShopifyApp::EmbeddedApp
-  # include ShopifyApp::RequireKnownShop
-  # include ShopifyApp::ShopAccessScopesVerification
+  helper_method :shop
 
-  helper_method :shop, :shop_domain, :shop_currency
+  def shop
+    @shop ||= Shop.find_by(shopify_domain: shop_domain)
+  end
 
   def shop_domain
     @shop_domain ||= shop_session.url
-    # request.env.dig('rack.request.query_hash', 'shop')
-  end
-
-  def shop
-    Shop.find_by(shopify_domain: shop_domain)
-  end
-
-  def shop_currency
-    begin
-      @shop_current_currency = shop.with_shopify_session do
-        money_format = ShopifyAPI::Shop.current.money_format
-        currency = ActionView::Base.full_sanitizer.sanitize(money_format).split("{").first.gsub(" ","")
-        currency = money_format.split("}").last.gsub(" ","") if currency == ''
-        currency
-      end
-    rescue
-      ''
-    end
   end
 
 end
--- app/javascript/lib/shopify_app.js
+++ app/javascript/lib/shopify_app.js
@@ -1,4 +1,3 @@
-console.log('START')
 document.addEventListener('DOMContentLoaded', () => {
   var data = document.getElementById('shopify-app-init').dataset;
   var AppBridge = window['app-bridge'];
@@ -14,21 +13,3 @@ document.addEventListener('DOMContentLoaded', () => {
     title: data.page,
   });
 });
-
-
-
-
-// var data = document.getElementById('shopify-app-init').dataset;
-// var AppBridge = window['app-bridge'];
-// var createApp = AppBridge.default;
-// window.app = createApp({
-//   apiKey: data.apiKey,
-//   // host: data.host,
-//   shopOrigin: data.shopOrigin,
-// });
-// console.log('11111111111111111111111')
-// var actions = AppBridge.actions;
-// var TitleBar = actions.TitleBar;
-// TitleBar.create(app, {
-//   title: data.page,
-// });
